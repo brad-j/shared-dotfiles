be0703695fbbe4aa0dfab81862be173e8351ee58
brad-j <brad@bradjohnson.io>
2025-03-08 20:52:31 -0500

    Tweaks

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -33,6 +33,15 @@ return {
           "xml",
           "yaml",
         },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+            node_incremental = "<Enter>",
+            scope_incremental = false,
+            node_decremental = "<Backspace>",
+          },
+        },
       })
     end
 }
