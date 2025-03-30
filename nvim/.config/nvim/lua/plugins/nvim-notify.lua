e44acfd930babf761aaaf561ce38b9cefefb3f65
brad-j <brad@bradjohnson.io>
2025-03-30 16:02:19 -0400

    nvim tweaks

--- nvim/.config/nvim/lua/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/plugins/nvim-notify.lua
@@ -1,3 +1,15 @@
 return {
-  'rcarriga/nvim-notify'
+  'rcarriga/nvim-notify',
+  opts = {
+    stages = 'fade',
+    timeout = 2000,
+    background_colour = '#000000',
+    max_width = 50,
+    max_height = 10,
+    render = 'compact',
+    fps = 60,
+    level = vim.log.levels.INFO,
+    minimum_width = 50,
+    minimum_height = 10,
+  },
 }
